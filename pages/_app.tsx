4ef1e3b66dc608c1d663a535f139e0964bf94936
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-03 22:05:10 +0300

    urql added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider, createClient } from 'urql';
+
+const client = createClient({ url: 'http://localhost:8099/graphql' })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return( 
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 export default MyApp
