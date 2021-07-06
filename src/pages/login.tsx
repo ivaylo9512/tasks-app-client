2b1edf078d35f9fcd273da397bc2e9fb615e3c3b
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-06 21:42:31 +0300

    login added

---
+++ src/pages/login.tsx
@@ -0,0 +1,43 @@
+import useInput from "../hooks/useInput"
+import Link from 'next/link'
+import { useRef, useMemo, useEffect } from "react";
+import { useQuery } from "urql";
+import { LoginDocument, LoginMutation, LoginMutationVariables, UserInput } from "../generated/graphql";
+import validateEmail from "../helpers/validateEmail";
+
+const Login = () => {
+    const loginInput = useRef<UserInput>();
+    const [usernameOrEmail, usernameOrEmailInput] = useInput({
+        placeholder: 'username or email', 
+        name: 'username'
+    });
+    const [password, passwordInput] = useInput({placeholder: 'password', name: 'password', type: 'password'});
+    const [loginValue, loginMut] = useQuery<LoginMutation, LoginMutationVariables>({ query: LoginDocument, pause: !loginInput.current, variables: loginInput.current, context: useMemo(() => ({ url: 'http://localhost:8056/graphql'}), [])})
+    
+    const login = () => {
+        validateEmail(usernameOrEmail)
+        loginInput.current = {username: usernameOrEmail, password}
+        loginMut();
+    }
+
+    useEffect(() => {
+
+    }, [loginValue])
+
+    return (
+        <section>
+            <form onSubmit={login}>
+                {usernameOrEmailInput}
+                {passwordInput}
+                <div className='errors'>
+                    {loginValue.data?.login.errors?.map(err => 
+                        <span>{err.message}</span>
+                    )}
+                </div>
+                <span>Don't have an account?<Link href="/register"> Sign up.</Link></span>
+                <button>login</button>
+            </form>
+        </section>
+    )
+}
+export default Login
