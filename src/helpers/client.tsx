d503b29c82c72c70d2d03256c49e4c322722e02c
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-12 23:58:28 +0300

    client swap + daily change

--- src/helpers/client.tsx
+++ src/helpers/client.tsx
@@ -1,50 +1,10 @@
-import {Resolver, Cache, query } from "@urql/exchange-graphcache";
-import {dedupExchange, cacheExchange, Exchange, fetchExchange, stringifyVariables, useQuery, makeOperation } from "urql";
-import { pipe, tap } from "wonka";
-import { authExchange } from '@urql/exchange-auth';
+import { NextPageContext } from "next";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-export const createClient = (ssrExchange: any ) => ({
-    url: 'http://localhost:8099/graphql',
-    exchanges: [dedupExchange, cacheExchange, ssrExchange, authExchange({
-        getAuth, willAuthError, addAuthToOperation}), fetchExchange]})
-
-const getAuth = async ({ authState } : {authState: {token: string, refreshToken : string | null}}) => {
-    console.log('here')
-    if (!authState) {
-      const token = localStorage.getItem('token');
-      const refreshToken = localStorage.getItem('refreshToken');
-      if (token && refreshToken) {
-        return { token, refreshToken };
-      }
-      return null;
-    }
-  
-    return null;
-};
-
-const addAuthToOperation = ({ authState, operation }) => {
-    if (!authState || !authState.token) {
-      return operation;
-    }
-  
-    const fetchOptions =
-      typeof operation.context.fetchOptions === 'function'
-        ? operation.context.fetchOptions()
-        : operation.context.fetchOptions || {};
-  
-    return makeOperation(operation.kind, operation, {
-      ...operation.context,
-      fetchOptions: {
-        ...fetchOptions,
-        headers: {
-          ...fetchOptions.headers,
-          Authorization: authState.token,
-        },
-      },
-    });
-  };
-
-const willAuthError = ({ authState }) => {
-    if (!authState) return true;
-    return false;
-  }
+export const createClient = (ctx: NextPageContext) =>
+  new ApolloClient({
+    uri: process.env.NEXT_PUBLIC_API_URL as string,
+    cache: new InMemoryCache({
+    
+    })
+  })
--- src/pages/daily.tsx
+++ src/pages/daily.tsx
@@ -1,12 +1,28 @@
-import CreateTask from "../components/CreateTask"
+import CreateTask from '../components/CreateTask'
+import { withUrqlClient } from 'next-urql';
+import Link from 'next/link';
+import Calendar from '../components/CalendarView';
+import styled from 'styled-components';
+import TaskView from '../components/TasksView';
+
+const Container = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+`
+const Section = styled.section`
+    height: 100%;
+`
 
 const daily = () => {
 
     return(
-        <section>
-            <TaskList />
-            <CreateTask />
-        </section>
+        <Section>
+            <Container>
+                <TaskView />
+            </Container>
+        </Section>
     )
 }
 export default daily 
