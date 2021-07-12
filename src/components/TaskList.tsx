5933945d161dbcd7bf8e94978c6468538e009039
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-12 19:24:27 +0300

    TaskList added

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    height: 100%;
+`
+const TaskList = () => {
+    return(
+        <Container>
+        </Container>
+    )
+}
+export default TaskList
