9ef13d28d6fcb44a69525267a3ebb50eb279de6a
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-16 23:58:07 +0300

    getting tasks with useTasksByDateQuery

--- src/pages/calendar-view.tsx
+++ src/pages/calendar-view.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect, createContext, useMemo } from "react";
 import Calendar from '../components/Calendar';
 import styled from 'styled-components';
 import TaskView from '../components/TasksView';
+import { withApollo } from "../helpers/create-with-apollo";
+import { useTasksByDateQuery, Task, useCreateTaskMutation, TasksByDateQuery, TasksByDateDocument } from "../generated/graphql";
 
 const Container = styled.div`
     height: 100%;
@@ -25,16 +27,33 @@ export const DateProvider = createContext<DateContext>({
 const CalendarView: React.FC = () => {
     const [date, setDate] = useState<Date>(() => new Date());
     const value = useMemo(() => ({date, setDate}), [date, setDate]);
+    const [dateString, setDateString] = useState<string>('');
+    const { data: tasks} = useTasksByDateQuery({
+        variables: {
+            date: dateString
+        },
+        skip: !dateString,
+        notifyOnNetworkStatusChange: true,
+    });
+
+    
+
+    useEffect(() => {
+        const utcDate = new Date(date);
+        utcDate.setMinutes(utcDate.getMinutes() - utcDate.getTimezoneOffset());
+        setDateString(utcDate.toISOString().split('T')[0]);
+    },[date])
+
 
     return(
         <DateProvider.Provider value={value}>
         <Section>
             <Container>
                 <Calendar date={date} setDate={setDate} />
-                <TaskView />
+                <TaskView tasks={tasks} />
             </Container>
         </Section>
         </DateProvider.Provider>
     )
 }
-export default CalendarView 
+export default withApollo({ssr: true})(CalendarView);
