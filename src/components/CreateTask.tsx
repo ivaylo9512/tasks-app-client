18c2d4aa11f164e3197c1596792c8e9bf64faf2c
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-17 21:37:59 +0300

    createTask inputs + buttons change

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -1,27 +1,56 @@
 import useInput from "../hooks/useInput"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBell } from '@fortawesome/free-solid-svg-icons'
+import { useState } from "react";
 
 const CreateTask: React.FC = () => {
-    const [note, noteInput] = useInput({
-        name: 'note',
-        placeholder: 'note',
-    })
-    const [from, fromInput] = useInput({
-        name: 'from',
-        initialValue: '00:00',
-        placeholder: 'from',
-    })
-    const [top, toInput] = useInput({
-        name: 'to',
-        initialValue: '23:00',
-    })
+    const [values, {nameInput, timeInput, alertAtInput}] = createInputs();
+    const [isAlertAtView, setIsAlertAtView] = useState<boolean>(false);
+    const setView = () => {
+        setIsAlertAtView(!isAlertAtView);
+    }
     return(
         <div>
-            {noteInput}
-
-            <FontAwesomeIcon icon={faBell} />
+            {isAlertAtView ? <>
+                {timeInput}
+                {alertAtInput} 
+                <button>back</button>
+            </> : <>
+                {nameInput}
+                <button>add</button>
+                <button onClick={setView}>
+                    <FontAwesomeIcon icon={faBell} />
+                </button>
+            </>}
         </div>
     )
+}
+type Values = {
+    name: string,
+    time: string,
+    alertAt: string
+}
+type Inputs = {
+    nameInput: JSX.Element,
+    timeInput: JSX.Element,
+    alertAtInput: JSX.Element
+}
+
+const createInputs = (): [Values, Inputs] => {
+    const [name, nameInput] = useInput({
+        name: 'name',
+        placeholder: 'name',
+    })
+    const [time, timeInput] = useInput({
+        name: 'time',
+        initialValue: '00:00',
+        type: 'time'
+    })
+    const [alertAt, alertAtInput] = useInput({
+        name: 'alertAt',
+        type: 'date'
+    })
+
+    return [{name, time, alertAt}, {nameInput, timeInput, alertAtInput}]
 }
 export default CreateTask
