3c10b00e09989925bd9bfb9a74acb6aa0fee7444
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-17 23:09:57 +0300

    toggle button + props types

--- src/components/TasksView.tsx
+++ src/components/TasksView.tsx
@@ -1,14 +1,27 @@
 import styled from "styled-components"
 import CreateTask from "./CreateTask"
 import TaskList from "./TaskList"
+import { TaskFragment } from "../generated/graphql"
 
 const Section = styled.section`
     flex:1;
+    overflow: hidden;
+    position:relative;
 `
-const TaskView: React.FC = () => {
+const ToggleButton = styled.button`
+    position: absolute;
+    right: 0;
+`
+type TaskViewProps = {
+    tasks?: TaskFragment[];
+    toggleCalendar: () => void,
+    isHidden: boolean
+}
+const TaskView: React.FC<TaskViewProps> = ({ isHidden, tasks, toggleCalendar }) => {
     return(
         <Section>
-            <TaskList />
+            <ToggleButton onClick={toggleCalendar}>toggle</ToggleButton>
+            <TaskList isHidden={isHidden} tasks={tasks}/>
             <CreateTask />
         </Section>
     )
