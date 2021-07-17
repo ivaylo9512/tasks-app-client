683b8b951e89afc31d42eeb6498d49d25b9a67cd
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-17 23:16:09 +0300

    styling + check for router

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -1,28 +1,51 @@
 import useInput from "../hooks/useInput"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBell } from '@fortawesome/free-solid-svg-icons'
+import { useCreateTaskMutation, TasksByDateDocument, TasksByDateQuery } from "../generated/graphql";
 import { useState } from "react";
+import styled from "styled-components";
+import { useRouter } from 'next/router'
+
+const Container = styled.div`
+    height: 11vh;
+    width: 87%;
+    margin: 0.5vh auto;
+    background: #000000;
+    border-style: groove;
+    border-color: white;
+    box-shadow: 0 0 0.2vw 0.05vw rgb(0 0 0);
+    display: flex;
+    border-radius: 1.9vw;
+    align-items: center;
+    justify-content: center;
+    color: white;
+`
 
 const CreateTask: React.FC = () => {
+    const {route} = useRouter();
     const [values, {nameInput, timeInput, alertAtInput}] = createInputs();
     const [isAlertAtView, setIsAlertAtView] = useState<boolean>(false);
+    
     const setView = () => {
         setIsAlertAtView(!isAlertAtView);
     }
+
     return(
-        <div>
+        <Container>
             {isAlertAtView ? <>
                 {timeInput}
                 {alertAtInput} 
-                <button>back</button>
+                <button onClick={setView}>back</button>
             </> : <>
                 {nameInput}
                 <button>add</button>
-                <button onClick={setView}>
-                    <FontAwesomeIcon icon={faBell} />
-                </button>
+                {route == 'calendar-view' && 
+                    <button onClick={setView}>
+                        <FontAwesomeIcon icon={faBell} />
+                    </button>
+                }
             </>}
-        </div>
+        </Container>
     )
 }
 type Values = {
@@ -53,4 +76,4 @@ const createInputs = (): [Values, Inputs] => {
 
     return [{name, time, alertAt}, {nameInput, timeInput, alertAtInput}]
 }
-export default CreateTask
+export default CreateTask;
